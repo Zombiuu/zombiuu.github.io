afe2bd0f1d76eeef1a0e9aecb952c13df648ed56
Ramsés Cayetano Diez Galvan <ramses.diez@live.u-tad.com>
2017-03-13 12:37:20 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ function myFunction(arr) {
         var p2 = document.createElement("p");
         var enlace = document.createElement("a");
         var nodoTitulo = document.createTextNode(arr[i].Titulo);
-        var nodoDesc = document.createTextNode(arr[i].Descripcion);
+
         var nodoEnlace = document.createTextNode(arr[i].Link);
         var nodoVisitar = document.createTextNode("Visitar página");
         var etiquetai = document.createElement("i");
@@ -39,7 +39,7 @@ function myFunction(arr) {
         //ATRIBUTOS//
         section.insertBefore(fila, footer);
         fila.appendChild(proyecto);
-        etiquetai.setAttribute("class", "fa fa-star fa-3");
+        etiquetai.setAttribute("class", "fa fa-star fa-3 destacado");
         etiquetai.setAttribute("aria-hidden", "true");
         etiquetai.setAttribute("id", "destacado");
         imagen.setAttribute("class", "img-circle");
@@ -50,12 +50,18 @@ function myFunction(arr) {
         enlace.setAttribute("target", "_blank");
         enlace.setAttribute("class", "btn btn-default");
         enlace.setAttribute("href", arr[i].Link);
-        proyecto.setAttribute("class", "col-lg-4");
+        //Disponible o no
+        if (arr[i].Disponible == false) {
+            proyecto.setAttribute("class", "col-lg-4 noDisp");
+        } else {
+            proyecto.setAttribute("class", "col-lg-4");
+        }
+
 
 
 
         titulo.appendChild(nodoTitulo);
-        p1.appendChild(nodoDesc);   
+
         enlace.appendChild(nodoVisitar);
         proyecto.appendChild(imagen);
         proyecto.appendChild(titulo);
@@ -68,11 +74,68 @@ function myFunction(arr) {
         p2.appendChild(enlace);
 
 
+        //INFO
+        var newarr = arr[i].Grupal
+       
+        var lista = document.createElement("ul");
+        var li1 = document.createElement("li");
+        var li2 = document.createElement("li");
+        var li3 = document.createElement("li");
+        var li4 = document.createElement("li");
+        var li5 = document.createElement("li");
+        var li6 = document.createElement("li");
+        var nodoFecha = document.createTextNode("Fecha: " + arr[i].Fecha);
+        var nodoResponsive = document.createTextNode("Responsive: " + arr[i].Responsive);
+        var nodoAsig = document.createTextNode("Asignatura: " + arr[i].Asignaturas);
+        var nodoVis = document.createTextNode("Resolución: " + arr[i].Visualizacion);
+        var nodoDesc = document.createTextNode(arr[i].Descripcion);
+
+        var comp = false;
+
+
+        for (j = 0; j < newarr.length; j++) { 
+            if (j == 0 ) {
+                if (newarr.length == 1) {
+                    var nodoGrupal = document.createTextNode("Autor/es: " + newarr[j].Nombre);
+                } else {
+                    var nodoGrupal = document.createTextNode("Autor/es: " + newarr[j].Nombre + ",");
+                }
+               
+                
+            } else {
+                var nodoGrupal = document.createTextNode(newarr[j].Nombre);
+                
+                
+            }
+
+            
+            lista.setAttribute("class", "lista");
+            p1.appendChild(lista);
+            lista.appendChild(li1);
+            lista.appendChild(li2);
+            lista.appendChild(li3);
+            lista.appendChild(li4);
+            lista.appendChild(li6);
+            lista.appendChild(li5);
+
+            li1.appendChild(nodoFecha);
+            li2.appendChild(nodoAsig);
+            li3.appendChild(nodoResponsive);
+            li4.appendChild(nodoVis);
+            li6.appendChild(nodoGrupal);
+            li5.appendChild(nodoDesc);
+        }
+
+
+
+
+
+
     }
 
 
 
- }
+}
 
 
 
